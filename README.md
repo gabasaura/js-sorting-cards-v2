dbcd74260dcea31f8224746d1dce59120dbd95ab
gabasaura <gabriela.garin@gmail.com>
2024-05-27 23:10:00 -0400

    correccion selectingSort log

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This project is a simple vanilla JavaScript application that Sort Cards (1 - 9). It demonstrates sorting method and provides a basic structure for further development.
+Vanilla JavaScript code that sort shuffle cards. It use the selecting sort method and provides a basic structure for further development.
--- main.js
+++ main.js
@@ -1,118 +1,110 @@
-window.onload = function() {
-    const suits = ["spade", "diamond", "heart", "club"];
-    const values = [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-  
-    // HTML CALLINGS
-    const drawButton = document.getElementById("drawButton");
-    const sortButton = document.getElementById("sortButton");
-    const numCardDrawInput = document.getElementById("numCardDrawInput");
-    const cardContainer = document.getElementById("cardContainer");
-    const sortedCardContainer = document.getElementById("sortedCardContainer");
-    const sortingStepsDiv = document.getElementById("sortingSteps");
-  
-    // TO GET RANDOM ITEM
-    function getRandomItem(array) {
-      return array[Math.floor(Math.random() * array.length)];
-    }
-  
-    // GET RANDOM CARD
-    function drawRandomCard() {
-      const randomSuit = getRandomItem(suits);
-      const randomValue = getRandomItem(values);
-      return { suit: randomSuit, value: randomValue };
-    }
-  
-    // DRAW CARDS
-    const drawnCards = [];
-    drawButton.addEventListener("click", function() {
+window.onload = function () {
+  const suits = ["♦", "♥", "♠", "♣"];
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+
+  function drawRandomCard() {
+      const suit = suits[Math.floor(Math.random() * suits.length)];
+      const value = values[Math.floor(Math.random() * values.length)];
+      return { suit: suit, value: value };
+  }
+
+  // HTML CALLINGS
+  const drawButton = document.getElementById("drawButton");
+  const sortButton = document.getElementById("sortButton");
+  const cardContainer = document.getElementById("cardContainer");
+  const sortedCardContainer = document.getElementById("sortedCardContainer");
+  const numCardDrawInput = document.getElementById("numCardDrawInput");
+  const sortingStepsDiv = document.getElementById("sortingStepsDiv");
+
+  // DRAW CARDS
+  const drawnCards = [];
+  drawButton.addEventListener("click", function () {
       const numCardsToDraw = parseInt(numCardDrawInput.value);
-      if (!isNaN(numCardsToDraw) && numCardsToDraw > 0 && numCardsToDraw <= 9) {
-        // Clear existing cards
-        cardContainer.innerHTML = "";
-        drawnCards.length = 0; // Clear drawn cards array
-  
-        // DRAW NEW
-        for (let i = 0; i < numCardsToDraw; i++) {
-          const randomCard = drawRandomCard();
-          drawnCards.push(randomCard);
-          const card = document.createElement("div");
-          card.className = "p-3 m-2 border border-success rounded-2 card";
-          card.classList.add(randomCard.suit);
-          card.innerHTML = `
-            <div class="topSymbol"></div>
-            <div class="num">${randomCard.value}</div>
-            <div class="bottomSymbol"></div>
-          `;
-          cardContainer.appendChild(card);
-        }
-      } else {
-        alert("Enter a number between 1 and 9.");
-      }
-    });
-  
-    // SORT CARDS
-    function sortCards(arr) {
-      const len = arr.length;
-      const log = []; 
-      sortingStepsDiv.innerHTML = "";
-  
-      for (let i = 0; i < len - 1; i++) {
-        let min = i;
-        for (let j = i + 1; j < len; j++) {
-          const valueAIndex = values.indexOf(arr[j].value);
-          const valueBIndex = values.indexOf(arr[min].value);
-  
-          if (valueAIndex < valueBIndex) {
-            min = j;
+      if (!isNaN(numCardsToDraw)) {
+          // Clear existing cards
+          cardContainer.innerHTML = "";
+          drawnCards.length = 0; // Clear drawn cards array
+
+          // DRAW NEW
+          for (let i = 0; i < numCardsToDraw; i++) {
+              const randomCard = drawRandomCard();
+              drawnCards.push(randomCard);
+              const card = document.createElement("div");
+              card.className = "p-2 m-2 border border-success rounded-2 card";
+              card.classList.add(randomCard.suit);
+              card.innerHTML = `
+                  <div class="topSymbol">${randomCard.suit}</div>
+                  <div class="num">${randomCard.value}</div>
+                  <div class="bottomSymbol">${randomCard.suit}</div>
+              `;
+              cardContainer.appendChild(card);
           }
-        }
-  
-        if (min !== i) {
-          const temp = arr[i];
-          arr[i] = arr[min];
-          arr[min] = temp;
-          sortingStepsDiv.innerHTML += `<div class="container text-center">${step}</div>`;
-        }
+      } else {
+          alert("duh");
       }
-      return arr;
-    }
-  
+  });
 
-   
+  // SELECTION SORT
+  const selectionSort = cards => {
+      const log = [];
 
+      for (let min = 0; min < cards.length - 1; min++) {
+          for (let i = min + 1; i < cards.length; i++) {
+              if (parseInt(cards[min].value) > parseInt(cards[i].value)) {
+                  const aux = cards[min];
+                  cards[min] = cards[i];
+                  cards[i] = aux;
 
-    // SORT BUTTON CLICK EVENT
-    sortButton.addEventListener("click", function() {
-      const result = sortCards([...drawnCards]); // Sort the drawn cards
-      const sortedCards = result; // Get the sorted cards array from the result
+                  // Log the state
+                  log.push({ state: [...cards] });
+                  console.log([...cards])
+              }
+          }
+      }
+
+      // Display log
+      sortingStepsDiv.innerHTML = ""; // Clear previous logs
+      log.forEach(step => {
+          const stepElement = document.createElement("div");
+          stepElement.className = "sorting-step d-flex";
+          const stepCards = document.createElement("div");
+          stepCards.className = "d-flex";
+          step.state.forEach(card => {
+              const cardElement = document.createElement("div");
+              cardElement.className = "p-2 m-2 border border-success rounded-2 card";
+              cardElement.classList.add(card.suit);
+              cardElement.innerHTML = `
+                  <div class="topSymbol">${card.suit}</div>
+                  <div class="num">${card.value}</div>
+                  <div class="bottomSymbol">${card.suit}</div>
+              `;
+              stepCards.appendChild(cardElement);
+          });
+
+          stepElement.appendChild(stepCards);
+          sortingStepsDiv.appendChild(stepElement);
+      });
+
+      return cards;
+  }
+
+  // SORT BUTTON CLICK EVENT
+  sortButton.addEventListener("click", function () {
+      const sortedCards = selectionSort([...drawnCards]); // Sort the drawn cards
       sortedCardContainer.innerHTML = "";
-  
+
       for (let i = 0; i < sortedCards.length; i++) {
-        const card = sortedCards[i];
-        const sortedCard = document.createElement("div");
-        sortedCard.className = "p-3 m-2 border border-success rounded-2 card";
-        sortedCard.classList.add(card.suit);
-        sortedCard.innerHTML = `
-          <div class="topSymbol"></div>
-          <div class="num">${card.value}</div>
-          <div class="bottomSymbol"></div>
-        `;
-        sortedCardContainer.appendChild(sortedCard);
+          const card = sortedCards[i];
+          const sortedCard = document.createElement("div");
+          sortedCard.className = "p-2 m-2 border border-success rounded-2 card";
+          sortedCard.classList.add(card.suit);
+          sortedCard.innerHTML = `
+              <div class="topSymbol">${card.suit}</div>
+              <div class="num">${card.value}</div>
+              <div class="bottomSymbol">${card.suit}</div>
+          `;
+          sortedCardContainer.appendChild(sortedCard);
       }
-    });
-  };
-  
+  });
+}
